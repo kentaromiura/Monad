4402dafc844546cec8b0d479dfe2f34ddf6c151a
Cristian Carlesso <kentaromiura@gmail.com>
2013-01-14 17:47:09 +0000

    Add basic project structure
    Thanks to @arian for the structure (copied from the Classy project)
    Add some tests.
    Fix a bug caused by me using a cell phone to input text on github site.

---
+++ Source/Monad.js
@@ -0,0 +1,20 @@
+// Monad.js MIT License, Cristian Carlesso @kentaromiura mykenta.blogspot.com
+//
+// I used the following resurces as a inspiration.
+// http://blogs.msdn.com/b/wesdyer/archive/2008/01/11/the-marvels-of-monads.aspx
+// http://stackoverflow.com/questions/2704652/monad-in-plain-english-for-the-oop-programmer-with-no-fp-background
+// Douglas Crockford: Monads and Gonads (YUIConf Evening Keynote) http://www.youtube.com/watch?v=dkZFtimgAcM
+
+function Monad(value){
+	if (value instanceof Monad) return value
+	this.value = function(){return value}
+}
+
+Monad.prototype.bind = function(f){
+	var value = this.value()
+	if (f === Monad || value === null || value === undefined) return this
+	if (typeof f === 'function') return new Monad(f(value))
+	return this
+}
+
+module.exports = Monad
